c51a468d04c8888bd498198cc9ea66ba8ff16483
Ali <aliabbas808000@gmail.com>
2024-09-28 23:32:41 +0200

    prep.js.week3

---
+++ prep-js-week3.js
@@ -0,0 +1,33 @@
+import { modules, students, mentors, classes } from "./hyf.js";
+
+/**
+ * Tjebbe would like help to get a list of possible mentors for a module.
+ * Fill in this function that finds all the mentors that can teach the given module.
+ *
+ * It should return an array of names. So something like:
+ *  ['John', 'Mary']
+ */
+const possibleMentorsForModule = (moduleName) => {
+const availableMentors = mentors.filter(mentor => mentor.canTeach.includes(moduleName))
+return availableMentors.map( mentor => mentor.name)
+  
+};
+
+console.log(possibleMentorsForModule('javascript'));
+
+/**
+ * Tjebbe wants to make it even easier for himself.
+ * Fill in this function that chooses a random mentor to teach the given module.
+ *
+ * It should return a single name.
+ */
+const findMentorForModule = (moduleName) => {
+const mentorsForModule = possibleMentorsForModule(moduleName)
+
+ const randomMentor =  Math.floor(Math.random( ) * mentorsForModule.length);
+ return mentorsForModule[randomMentor];
+
+  
+};
+
+console.log(findMentorForModule('browsers')); 
